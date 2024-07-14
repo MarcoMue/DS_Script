9837a6ffdeec050253ae87029aca3c461e063ec7
Marco M <marco.071094@gmail.com>
2024-07-14 18:25:13 +0200

    test

--- myscript.js
+++ myscript.js
@@ -87,12 +87,12 @@ $.getScript(
     const scriptInfo = twSDK.scriptInfo();
     const isValidScreen = twSDK.checkValidLocation("screen");
 
-    // https://zz2.tribalwars.works/game.php?village=2686&screen=info_player&ajax=fetch_villages&player_id=1483900
+    // https://de221.die-staemme.de/guest.php?screen=info_player&ajax=fetch_villages&player_id=1577168517
+    // https://zz2.tribalwars.works/game.php?screen=info_player&ajax=fetch_villages&player_id=1483900
     jQuery
-      .ajax({
-        url: `/game.php?village=8914&screen=info_player&ajax=fetch_villages&player_id=1577303441`,
-        dataType: "json",
-      })
+      .ajax(
+        `/game.php?screen=info_player&ajax=fetch_villages&player_id=1577303441`
+      )
       .done((response) => {
         const { no_authorization } = response;
 
