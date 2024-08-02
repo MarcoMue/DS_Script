19ac4c85addf41718ab25a55080190326fed579a
Marco M <marco.071094@gmail.com>
2024-08-02 20:10:40 +0200

    chore: Update script loading mechanism and UI for tribe troop counter

--- myscript2.js
+++ myscript2.js
@@ -1,6 +1,97 @@
 // User Input
-if (typeof DEBUG !== "boolean") DEBUG = false;
+// Script Config
+var scriptConfig = {
+  scriptData: {
+    prefix: "getIncsForPlayer",
+    name: "Get Incs for Player",
+    version: "v1.0.0",
+    author: "",
+    authorUrl: "",
+    helpLink: "",
+  },
+  translations: {
+    en_DK: {
+      "Get Incs for Player": "Get Incs for Player",
+      Help: "Help",
+      "There was an error!": "There was an error!",
+      "Script must be executed from Player Info screen!":
+        "Script must be executed from Player Info screen!",
+      Fetching: "Fetching",
+      "Fetching incomings for each village...":
+        "Fetching incomings for each village...",
+      "Error fetching village incomings!": "Error fetching village incomings!",
+      "Total Attacks:": "Total Attacks:",
+      "Total Large Attacks:": "Total Large Attacks:",
+      "Total Noble Attacks:": "Total Noble Attacks:",
+      "Total Villages:": "Total Villages:",
+      "Average attacks per village:": "Average attacks per village:",
+      "Could not find villages being attacked!":
+        "Could not find villages being attacked!",
+      "Player:": "Player:",
+      Village: "Village",
+      Coords: "Coords",
+      "Total Medium Attacks:": "Total Medium Attacks:",
+      "Total Small Attacks:": "Total Small Attacks:",
+      Players: "Players",
+      "You can't sort elements if any one is a descendant of another.":
+        "You can't sort elements if any one is a descendant of another.",
+      "sort this column": "sort this column",
+    },
+    fr_FR: {
+      "Get Incs for Player": "Récupérer les attaques entrantes pour le Joueur",
+      Help: "Aide",
+      "There was an error!": "There was an error!",
+      "Script must be executed from Player Info screen!":
+        "Le script doit être executé depuis le profil d'un joueur!",
+      Fetching: "Chargement",
+      "Fetching incomings for each village...":
+        "Chargement des ordres pour chaque village ...",
+      "Error fetching village incomings!":
+        "Erreur dans le chargement des ordres!",
+      "Total Attacks:": "Total - Attaque envoyées:",
+      "Total Large Attacks:": "Total - Attaque de grande envergure:",
+      "Total Noble Attacks:": "Total - Attaque de noble:",
+      "Total Villages:": "Total - Villages:",
+      "Average attacks per village:": "Moyenne - Attaque par village:",
+      "Could not find villages being attacked!":
+        "Impossible de trouver les villages attaqués!",
+      "Player:": "Joueur:",
+      Village: "Village",
+      Coords: "Coordonnées",
+      "Total Medium Attacks:": "Total - Attaque de moyenne envergure:",
+      "Total Small Attacks:": "Total - Attaque de petite envergure::",
+      Players: "Joueurs",
+      "You can't sort elements if any one is a descendant of another.":
+        "You can't sort elements if any one is a descendant of another.",
+      "sort this column": "sort this column",
+    },
+  },
+  allowedMarkets: [],
+  allowedScreens: ["info_player"],
+  allowedModes: [],
+  isDebug: DEBUG,
+  enableCountApi: true,
+};
 
 (function () {
   console.log("IIFE called.");
+
+  function openUI() {
+    html =
+      '<head></head><body><h1>Tribe troop counter</h1><form><fieldset><legend>Settings</legend><p><input type="radio" name="mode" id="of" value="Read troops of the village" onchange="setMode(\'members_troops\')">Read troops of the village</input></p><p><input type="radio" name="mode" id="in" value="Read defenses in the village" onchange="setMode(\'members_defense\')">Read defenses in the village</input></p></fieldset><fieldset><legend>Filters</legend><select id="variable"><option value="x">x</option><option value="y">y</option>' +
+      createUnitOption() +
+      '</select><select id="kind"><option value=">">></option><option value="<"><</option></select><input type="text" id="value"></input><input type="button" class="btn evt-confirm-btn btn-confirm-yes" onclick="addFilter()" value="Save filter"></input><p><table><tr><th>Variable filtered</th><th>Operatore</th><th>Value</th><th></th></tr>' +
+      createFilterTable() +
+      '</form></p></fieldset><div><p><input type="button" class="btn evt-confirm-btn btn-confirm-yes" id="run" onclick="readData()" value="Read data"></input></p></div></body>';
+    Dialog.show("Troop counter", html);
+    if (localStorage.troopCounterMode) {
+      if (localStorage.troopCounterMode == "members_troops") {
+        document.getElementById("of").checked = true;
+      } else {
+        document.getElementById("in").checked = true;
+      }
+    } else {
+      document.getElementById("of").checked = true;
+    }
+  }
 })();
