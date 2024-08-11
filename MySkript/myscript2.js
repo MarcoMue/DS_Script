f0311772025fa7c372115d214c310687894bbaf6
Marco M <marco.071094@gmail.com>
2024-08-11 02:00:50 +0200

    refactor: Optimize table creation in myscript2.js

--- MySkript/myscript2.js
+++ MySkript/myscript2.js
@@ -134,34 +134,22 @@ var scriptConfig = {
   }
 
   function loadWBCode() {
-    let wbString = `
-    6686&1961&snob&1721768099000&8&false&true&spear=/sword=/axe=MTYwMA==/archer=/spy=/light=ODAw/marcher=/heavy=/ram=/catapult=/knight=/snob=/militia=MA==
-    9849&1961&snob&1721768100000&8&false&true&spear=/sword=/axe=/archer=/spy=/light=/marcher=/heavy=/ram=/catapult=/knight=/snob=/militia=MA==
-    8543&1961&snob&1721768098000&8&false&false&spear=/sword=/axe=MTYwMA==/archer=/spy=/light=ODAw/marcher=/heavy=/ram=/catapult=/knight=/snob=/militia=MA==
-    8735&1961&snob&1721768099000&8&false&false&spear=/sword=/axe=/archer=/spy=/light=/marcher=/heavy=/ram=/catapult=/knight=/snob=/militia=MA==
-    9899&1961&ram&1721768097000&8&false&true&spear=/sword=/axe=MTYwMA==/archer=/spy=/light=ODAw/marcher=/heavy=/ram=/catapult=/knight=/snob=/militia=MA==
-  `;
-
     let data = document.getElementById("urlvalue").value;
-
-    let result = null;
-    // if (wbString) {
-    //   console.log("static");
-    //   result = convertWBPlanToArray(wbString);
-    // }
+    let results = null;
 
     if (data) {
-      result = convertWBPlanToArray(data);
+      results = convertWBPlanToArray(data);
     }
 
     // Function to add a new row to the table
     function addRow() {
-      if (result.length === 0) {
+      if (results.length === 0) {
         clearInterval(intervalId);
         return;
       }
 
-      const rowData = result.shift(); // Get the first element and remove it from the array
+      console.log("result: ", results);
+      const rowData = results.shift(); // Get the first element and remove it from the array
       console.log("rowData: ", rowData);
 
       const newRow = `
@@ -182,130 +170,6 @@ var scriptConfig = {
     $(document).on("click", ".removeRow", function () {
       $(this).closest("tr").remove();
     });
-  }
-
-  function readData() {
-    console.log("readData");
-
-    if (game_data.mode == "members") {
-      var html =
-        '<label> Reading...     </label><progress id="bar" max="1" value="0">  </progress>';
-      Dialog.show("Progress bar", html);
-      filtres = {};
-      if (localStorage.troopCounterFilter) {
-        filtres = JSON.parse(localStorage.troopCounterFilter);
-      }
-      table = document.getElementsByClassName("vis");
-      nMembers = table[2].rows.length;
-      playerInfoList = [];
-      for (i = 1; i < nMembers - 1; i++) {
-        let playerId = table[2].rows[i].innerHTML.split("[")[1].split("]")[0];
-        let villageAmount = table[2].rows[i].innerHTML
-          .split('<td class="lit-item">')[4]
-          .split("</td>")[0];
-        playerInfoList.push({
-          playerId: playerId,
-          villageAmount: villageAmount,
-        });
-      }
-      mode = localStorage.troopCounterMode;
-      data = "Coords,Player,";
-      unitsList = game_data.units;
-      for (k = 0; k < unitsList.length; k++) {
-        data = data + unitsList[k] + ",";
-      }
-      players = getPlayerDict();
-      data = data + "\n";
-      i = 0;
-      let pageNumber = 1;
-      (function loop() {
-        page = $.ajax({
-          url:
-            "https://" +
-            window.location.host +
-            "/game.php?screen=ally&mode=" +
-            mode +
-            "&player_id=" +
-            playerInfoList[i].playerId +
-            "&page=" +
-            pageNumber,
-          async: false,
-          function(result) {
-            return result.responseText;
-          },
-        });
-        document.getElementById("bar").value = i / playerInfoList.length;
-
-        let temp = page.responseText.split("vis w100");
-
-        if (temp.length === 2 || temp.length === 4) {
-          rows = page.responseText
-            .split("vis w100")
-            [temp.length - 1].split("<tr>");
-          step = 1;
-          if (mode == "members_defense") {
-            step = 2;
-          }
-          for (j = 2; j + step < rows.length; j = j + step) {
-            villageData = {};
-
-            let coords = rows[j].match(/\d{1,3}\|\d{1,3}/g)[0].split("|");
-            villageData["x"] = coords[0];
-            villageData["y"] = coords[1];
-            units = rows[j].split(/<td class="">|<td class="hidden">/g);
-            for (k = 1; k < units.length; k++) {
-              villageData[unitsList[k - 1]] = units[k]
-                .split("</td>")[0]
-                .replace(/ /g, "")
-                .replace(/\n/g, "")
-                .replace(/<spanclass="grey">\.<\/span>/g, "");
-            }
-            filtered = true; //filtered==true ok, ==false hide
-            for (key in filtres) {
-              for (k = 0; k < filtres[key].length; k++) {
-                if (filtres[key][k][0] === ">") {
-                  if (
-                    parseInt(villageData[key]) < parseInt(filtres[key][k][1])
-                  ) {
-                    filtered = false;
-                  }
-                } else if (filtres[key][k][0] === "<") {
-                  if (
-                    parseInt(villageData[key]) > parseInt(filtres[key][k][1])
-                  ) {
-                    filtered = false;
-                  }
-                }
-              }
-            }
-            if (filtered) {
-              data = data + villageData["x"] + "|" + villageData["y"] + ",";
-              data = data + players[playerInfoList[i].playerId] + ",";
-              for (k = 0; k < unitsList.length; k++) {
-                data = data + villageData[unitsList[k]] + ",";
-              }
-              data = data + "\n";
-            }
-          }
-        }
-        i++;
-
-        if (temp.length === 4) {
-          if (playerInfoList[i].villageAmount / 1000 > pageNumber) {
-            i--;
-            pageNumber++;
-          } else {
-            pageNumber = 1;
-          }
-        }
-
-        if (i < playerInfoList.length) {
-          setTimeout(loop, 200);
-        } else {
-          showData(data, mode);
-        }
-      })();
-    }
   }
 
   function parseBool(input) {
