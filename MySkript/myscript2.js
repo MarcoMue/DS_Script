ae2e39c9befeda8af5edb241d04a5fcb2fbdfcc9
Marco M <marco.071094@gmail.com>
2024-08-11 03:08:14 +0200

    refactor: Optimize plan array creation and table creation in myscript2.js

--- MySkript/myscript2.js
+++ MySkript/myscript2.js
@@ -1,4 +1,5 @@
 if (typeof DEBUG !== "boolean") DEBUG = false;
+DEBUG = true;
 
 // User Input
 // Script Config
@@ -148,10 +149,7 @@ var scriptConfig = {
         return;
       }
 
-      console.log("result: ", results);
       const rowData = results.shift(); // Get the first element and remove it from the array
-      console.log("rowData: ", rowData);
-
       const newRow = `
             <tr>
               <td>${rowData.commandId}</td>
@@ -187,16 +185,6 @@ var scriptConfig = {
 
   function convertWBPlanToArray(plan) {
     console.log("convertWBPlanToArray called.");
-
-    // 6963&12246&snob&1722632340000&8&false&false&spear=/sword=/axe=/archer=/spy=/light=/marcher=/heavy=/ram=/catapult=/knight=/snob=/militia=MA==
-    // 8031&12246&ram&1722632339000&8&false&false&spear=/sword=/axe=/archer=/spy=/light=/marcher=/heavy=/ram=/catapult=/knight=/snob=/militia=MA==
-    // 7587&12246&ram&1722632339000&8&false&false&spear=/sword=/axe=/archer=/spy=/light=/marcher=/heavy=/ram=/catapult=/knight=/snob=/militia=MA==
-    // 9658&12246&snob&1722632340000&8&false&false&spear=/sword=/axe=/archer=/spy=/light=/marcher=/heavy=/ram=/catapult=/knight=/snob=/militia=MA==
-    // 9123&12246&ram&1722632340000&8&false&false&spear=/sword=/axe=/archer=/spy=/light=/marcher=/heavy=/ram=/catapult=/knight=/snob=/militia=MA==
-    // 9038&12246&snob&1722632340000&8&false&false&spear=/sword=/axe=/archer=/spy=/light=/marcher=/heavy=/ram=/catapult=/knight=/snob=Mg==/militia=MA==
-
-    // 6087&5307&snob&1723270500000&11&false&false&spear=/sword=/axe=/archer=/spy=/light=/marcher=/heavy=/ram=/catapult=/knight=/snob=/militia=MA==
-    // 7587&9038&spy&1723339738123&24&false&false&spear=MQ==/sword=Mg==/axe=Mw==/archer=/spy=NA==/light=NQ==/marcher=/heavy=Ng==/ram=Nw==/catapult=OA==/knight=/snob=OQ==/militia=MA==
 
     const pattern = /==(?=[^\/])/g;
     // Using the pattern to split and keep the delimiter
@@ -241,5 +229,89 @@ var scriptConfig = {
 
     if (DEBUG) console.debug(`Plan objects created: `, planObjects);
     return planObjects;
+  }
+
+  function readData() {
+    console.log("readData called.");
+
+    $.get(
+      // $(".village_anchor").first().find("a").first().attr("href"),
+      "/game.php?village=6087&screen=info_village&id=7346",
+      function (html) {
+        var $cc = $(html).find(".commands-container");
+        if ($cc.length > 0) {
+          var w =
+            game_data.screen == "map"
+              ? "100%"
+              : $("#content_value").width() -
+                $('form[action*="action=command"]')
+                  .find("table")
+                  .first()
+                  .width() -
+                10 +
+                "px";
+          $('form[action*="action=command"]')
+            .find("table")
+            .first()
+            .css("float", "left")
+            .find("tr")
+            .last()
+            .after('<tr><td>Verstuur:</td><td class="sendTime">-</td>')
+            .closest("table")
+            .after(
+              $cc.find("table").parent().html() +
+                '<br><div style="clear:both;"></div>'
+            )
+            .next()
+            .css({
+              float: "right",
+              width: w,
+              display: "block",
+              "max-height": $('form[action*="action=command"]')
+                .find("table")
+                .first()
+                .height(),
+              overflow: "auto",
+            })
+            .find("tr.command-row")
+            .on("click", function () {
+              var $this = $(this),
+                duration = $('form[action*="action=command"]')
+                  .find("table")
+                  .find('td:contains("Duur:")')
+                  .next()
+                  .text()
+                  .trim()
+                  .split(":"),
+                sendTime =
+                  parseInt($this.find("span.timer").data("endtime")) -
+                  (parseInt(duration[0]) * 3600 +
+                    parseInt(duration[1]) * 60 +
+                    parseInt(duration[2]));
+              $this.closest("table").find("td").css("background-color", "");
+              $this.find("td").css("background-color", "#FFF68F");
+              $(".sendTime").html(
+                formatTimes(sendTime) +
+                  ' (<span class="sendTimer" data-endtime="' +
+                  sendTime +
+                  '"></span>)'
+              );
+              Timing.tickHandlers.timers.initTimers("sendTimer");
+              document.title = formatTimes(sendTime);
+            })
+            .filter(function () {
+              return (
+                $('img[src*="/return_"], img[src*="/back.png"]', this).length >
+                0
+              );
+            })
+            .remove();
+          $(".widget-command-timer").addClass("timer");
+          Timing.tickHandlers.timers.initTimers("widget-command-timer");
+        } else {
+          UI.ErrorMessage("No commands found");
+        }
+      }
+    );
   }
 })();
