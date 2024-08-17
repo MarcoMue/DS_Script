52f442d0d1bb2a8eb3fe4df36b96c40b3dd725a0
Marco M <marco.071094@gmail.com>
2024-08-17 20:40:16 +0200

    refactor: Optimize table creation and remove commented code in myscript2.js

--- MySkript/myscript2.js
+++ MySkript/myscript2.js
@@ -149,250 +149,15 @@ window.twSDK = {
       return string;
     }
   },
-};
-
-let intervalId;
-let results = [];
-let commands = [];
-let villiges = [""];
-
-(function () {
-  console.log("IIFE called.");
-  openUI();
-
-  function openUI() {
-    const html = `   
-        <h1>All Incs</h1>
-        <div>
-        <form>
-          <fieldset>
-            <legend>Settings</legend>
-            <p>
-              <input type="radio" name="mode" id="of" value="Read troops of the village">
-              Read troops of the village
-            </p>
-            <p>
-              <input type="radio" name="mode" id="in" value="Read defenses in the village">
-              Read defenses in the village
-            </p>
-          </fieldset>
-          <fieldset>
-            <legend>Filters</legend>
-            <input type="text" id="urlvalue">
-            <input type="button" class="btn evt-confirm-btn btn-confirm-yes" id="loadPlannerBtn" value="Load Planner">
-            <p>
-              <table id="myTable">
-                <tr>
-                  <th>First</th>
-                  <th>Two</th>
-                  <th>Three</th>
-                  <th>Last</th>
-                </tr>
-              </table>
-            </p>
-          </fieldset>
-          <div>
-            <p>
-              <input type="button" class="btn evt-confirm-btn btn-confirm-yes" id="run" value="Read data">
-            </p>
-          </div>
-        </form>
-      </div>
-    `;
-    // Dialog.show("Troop counter", html);
-
-    $("#contentContainer").eq(0).prepend(html);
-    $("#mobileContent").eq(0).prepend(html);
-    Timing.tickHandlers.timers.init();
-
-    document
-      .getElementById("of")
-      .addEventListener("change", () => setMode("members_troops"));
-    document
-      .getElementById("in")
-      .addEventListener("change", () => setMode("members_defense"));
-    document
-      .getElementById("loadPlannerBtn")
-      .addEventListener("click", loadWBCode);
-    document.getElementById("run").addEventListener("click", readData);
-  }
-
-  function addRow(row) {
-    // if (results.length === 0) {
-    //   clearInterval(intervalId);
-    //   return;
-    // }
-
-    // const rowData = results.shift(); // Get the first element and remove it from the array
-    // const newRow = `
-    //       <tr>
-    //         <td>${rowData.commandId}</td>
-    //         <td>${rowData.originVillageId}</td>
-    //         <td>${rowData.targetVillageId}</td>
-    //         <td><button class="removeRow">Remove</button></td>
-    //       </tr>
-    //       `;
-
-    $("#myTable").append(row);
-  }
-
-  function loadWBCode() {
-    let data = document.getElementById("urlvalue").value;
-
-    if (data) {
-      results = convertWBPlanToArray(data);
-      addRow(results);
+  getVillageIDByCoords: function (x, y) {
+    const xy = parseInt(`${x}${y}`, 10);
+    const village = TWMap.villages[xy];
+    if (!village) {
+      return NaN;
     }
-
-    // Set an interval to add a new row every second
-    // intervalId = setInterval(addRow, 250);
-
-    // Event delegation to handle row removal
-    $(document).on("click", ".removeRow", function () {
-      $(this).closest("tr").remove();
-    });
-  }
-
-  function parseBool(input) {
-    if (typeof input === "string") {
-      return input.toLowerCase() === "true";
-    } else if (typeof input === "boolean") {
-      return input;
-    } else {
-      console.error(
-        `${scriptInfo}: Invalid input: needs to be a string or boolean.`
-      );
-      return false;
-    }
-  }
-
-  function convertWBPlanToArray(plan) {
-    console.log("convertWBPlanToArray called.");
-
-    const pattern = /==(?=[^\/])/g;
-    // Using the pattern to split and keep the delimiter
-    const planArray = plan
-      .replace(/\s+/g, "") // Remove all spaces
-      .split(pattern)
-      .map((part, index, array) =>
-        index < array.length - 1 ? part + "==" : part
-      );
-
-    let planObjects = [];
-
-    for (let i = 0; i < planArray.length; i++) {
-      let planParts = planArray[i].split("&");
-      let units = planParts[7].split("/").reduce((obj, str) => {
-        if (!str) {
-          return obj;
-        }
-        const [unit, value] = str.split("=");
-        if (unit === undefined || value === undefined) {
-          return obj;
-        }
-        obj[unit] = parseInt(atob(value));
-        return obj;
-      }, {});
-
-      let planObject = {
-        commandId: i.toString(),
-        originVillageId: parseInt(planParts[0]),
-        targetVillageId: parseInt(planParts[1]),
-        slowestUnit: planParts[2],
-        arrivalTimestamp: parseInt(planParts[3]),
-        type: parseInt(planParts[4]),
-        drawIn: parseBool(planParts[5]),
-        sent: parseBool(planParts[6]),
-        units: units,
-      };
-
-      planObjects.push(planObject);
-      if (DEBUG) console.debug(`Plan object ${i} created: `, planObject);
-    }
-
-    if (DEBUG) console.debug(`Plan objects created: `, planObjects);
-    return planObjects;
-  }
-
-  function readData() {
-    console.log("readData called.");
-    let items = []; // Example items
-    fetchWorldData();
-
-    $.get(
-      // $(".village_anchor").first().find("a").first().attr("href"),
-      "/game.php?screen=info_village&id=2087",
-      function (html) {
-        let $cc = $(html).find(".commands-container");
-        if ($cc.length > 0) {
-          // <form id="command-data-form" action="/game.php?village=6963&amp;screen=place&amp;action=command" method="post" onsubmit="this.submit.disabled=true;" style="min-width: 800px">
-          // $('form[action*="action=command"]')
-          //   .find("table")
-          //   .first()
-          //   .css("float", "left")
-          //   .find("tr")
-          //   .last()
-          //   .after($cc.find("table").parent().html());
-
-          $cc
-            .find("table")
-            .first()
-            .find(".quickedit-out")
-            .each(function () {
-              // console.log($(this).attr("data-id"));
-              let commandID = $(this).attr("data-id");
-              console.log(commandID);
-              items.push(commandID);
-            });
-
-          $cc
-            .find("table")
-            .first()
-            .find(".command-row")
-            .each(function () {
-              commands.push($(this));
-              addRow($(this));
-            });
-
-          let timerId = setInterval(function () {
-            // Step 3: Fetch and process the item
-            if (items.length > 0) {
-              let item = items.shift(); // Fetch the first item
-              console.log("Processing:", item); // Process the item (example: log it)
-              jQuery
-                .ajax({
-                  url: `/game.php?screen=info_command&ajax=details&id=${item}`,
-                  dataType: "json",
-                })
-                .done((response) => {
-                  const { no_authorization } = response;
-                  if (no_authorization) {
-                    console.error(`Error:`, data);
-                  } else {
-                    console.log(response);
-                    results.push(response);
-                    // addRow(response);
-                  }
-                })
-                .fail((textStatus, errorThrown) => {
-                  console.error(
-                    `Request failed: ${textStatus}, ${errorThrown}`
-                  );
-                });
-            } else {
-              // Step 4: Clear the interval when the array is empty
-              clearInterval(timerId);
-              console.log("All items processed.");
-            }
-          }, 400);
-        } else {
-          UI.ErrorMessage("No commands found", $cc);
-        }
-      }
-    );
-  }
-
-  async function worldDataAPI(entity) {
+    return village.id;
+  },
+  worldDataAPI: async function (entity) {
     const TIME_INTERVAL = 60 * 60 * 1000; // fetch data every hour
     const LAST_UPDATED_TIME = localStorage.getItem(`${entity}_last_updated`);
 
@@ -669,15 +434,264 @@ let villiges = [""];
     worldData[entity] = objectToArray(worldData[entity], entity);
 
     return worldData[entity];
-  }
-
-  async function fetchWorldData() {
+  },
+  fetchWorldData: async function () {
     try {
-      const villages = await worldDataAPI("village");
+      const villages = await twSDK.worldDataAPI("village");
       return { villages };
     } catch (error) {
       UI.ErrorMessage(error);
       console.error(`${scriptInfo} Error:`, error);
     }
+  },
+};
+
+let intervalId;
+let results = [];
+let commands = [];
+let villiges = [""];
+
+(function () {
+  console.log("IIFE called.");
+  openUI();
+
+  function openUI() {
+    const html = `   
+        <h1>All Incs</h1>
+        <div>
+        <form>
+          <fieldset>
+            <legend>Settings</legend>
+            <p>
+              <input type="radio" name="mode" id="of" value="Read troops of the village">
+              Read troops of the village
+            </p>
+            <p>
+              <input type="radio" name="mode" id="in" value="Read defenses in the village">
+              Read defenses in the village
+            </p>
+          </fieldset>
+          <fieldset>
+            <legend>Filters</legend>
+            <input type="text" id="urlvalue">
+            <input type="button" class="btn evt-confirm-btn btn-confirm-yes" id="loadPlannerBtn" value="Load Planner">
+            <p>
+              <table id="myTable">
+                <tr>
+                  <th>First</th>
+                  <th>Two</th>
+                  <th>Three</th>
+                  <th>Last</th>
+                </tr>
+              </table>
+            </p>
+          </fieldset>
+          <div>
+            <p>
+              <input type="button" class="btn evt-confirm-btn btn-confirm-yes" id="run" value="Read data">
+            </p>
+            <p>
+              <input type="button" class="btn evt-confirm-btn btn-confirm-yes" id="update" value="Load DB Data">
+            </p>
+
+          </div>
+        </form>
+      </div>
+    `;
+    // Dialog.show("Troop counter", html);
+
+    $("#contentContainer").eq(0).prepend(html);
+    $("#mobileContent").eq(0).prepend(html);
+    Timing.tickHandlers.timers.init();
+
+    document
+      .getElementById("of")
+      .addEventListener("change", () => setMode("members_troops"));
+    document
+      .getElementById("in")
+      .addEventListener("change", () => setMode("members_defense"));
+    document
+      .getElementById("loadPlannerBtn")
+      .addEventListener("click", loadWBCode);
+    document.getElementById("run").addEventListener("click", readData);
+    document.getElementById("update").addEventListener("click", updateDB);
+  }
+
+  function addRow(row) {
+    // if (results.length === 0) {
+    //   clearInterval(intervalId);
+    //   return;
+    // }
+
+    // const rowData = results.shift(); // Get the first element and remove it from the array
+    // const newRow = `
+    //       <tr>
+    //         <td>${rowData.commandId}</td>
+    //         <td>${rowData.originVillageId}</td>
+    //         <td>${rowData.targetVillageId}</td>
+    //         <td><button class="removeRow">Remove</button></td>
+    //       </tr>
+    //       `;
+
+    $("#myTable").append(row);
+  }
+
+  function loadWBCode() {
+    let data = document.getElementById("urlvalue").value;
+
+    if (data) {
+      results = convertWBPlanToArray(data);
+      addRow(results);
+    }
+
+    // Set an interval to add a new row every second
+    // intervalId = setInterval(addRow, 250);
+
+    // Event delegation to handle row removal
+    $(document).on("click", ".removeRow", function () {
+      $(this).closest("tr").remove();
+    });
+  }
+
+  function parseBool(input) {
+    if (typeof input === "string") {
+      return input.toLowerCase() === "true";
+    } else if (typeof input === "boolean") {
+      return input;
+    } else {
+      console.error(
+        `${scriptInfo}: Invalid input: needs to be a string or boolean.`
+      );
+      return false;
+    }
+  }
+
+  function convertWBPlanToArray(plan) {
+    console.log("convertWBPlanToArray called.");
+
+    const pattern = /==(?=[^\/])/g;
+    // Using the pattern to split and keep the delimiter
+    const planArray = plan
+      .replace(/\s+/g, "") // Remove all spaces
+      .split(pattern)
+      .map((part, index, array) =>
+        index < array.length - 1 ? part + "==" : part
+      );
+
+    let planObjects = [];
+
+    for (let i = 0; i < planArray.length; i++) {
+      let planParts = planArray[i].split("&");
+      let units = planParts[7].split("/").reduce((obj, str) => {
+        if (!str) {
+          return obj;
+        }
+        const [unit, value] = str.split("=");
+        if (unit === undefined || value === undefined) {
+          return obj;
+        }
+        obj[unit] = parseInt(atob(value));
+        return obj;
+      }, {});
+
+      let planObject = {
+        commandId: i.toString(),
+        originVillageId: parseInt(planParts[0]),
+        targetVillageId: parseInt(planParts[1]),
+        slowestUnit: planParts[2],
+        arrivalTimestamp: parseInt(planParts[3]),
+        type: parseInt(planParts[4]),
+        drawIn: parseBool(planParts[5]),
+        sent: parseBool(planParts[6]),
+        units: units,
+      };
+
+      planObjects.push(planObject);
+      if (DEBUG) console.debug(`Plan object ${i} created: `, planObject);
+    }
+
+    if (DEBUG) console.debug(`Plan objects created: `, planObjects);
+    return planObjects;
+  }
+
+  function readData() {
+    console.log("readData called.");
+    let items = []; // Example items
+
+    $.get(
+      // $(".village_anchor").first().find("a").first().attr("href"),
+      "/game.php?screen=info_village&id=2087",
+      function (html) {
+        let $cc = $(html).find(".commands-container");
+        if ($cc.length > 0) {
+          // <form id="command-data-form" action="/game.php?village=6963&amp;screen=place&amp;action=command" method="post" onsubmit="this.submit.disabled=true;" style="min-width: 800px">
+          // $('form[action*="action=command"]')
+          //   .find("table")
+          //   .first()
+          //   .css("float", "left")
+          //   .find("tr")
+          //   .last()
+          //   .after($cc.find("table").parent().html());
+
+          $cc
+            .find("table")
+            .first()
+            .find(".quickedit-out")
+            .each(function () {
+              // console.log($(this).attr("data-id"));
+              let commandID = $(this).attr("data-id");
+              console.log(commandID);
+              items.push(commandID);
+            });
+
+          $cc
+            .find("table")
+            .first()
+            .find(".command-row")
+            .each(function () {
+              commands.push($(this));
+              addRow($(this));
+            });
+
+          let timerId = setInterval(function () {
+            // Step 3: Fetch and process the item
+            if (items.length > 0) {
+              let item = items.shift(); // Fetch the first item
+              console.log("Processing:", item); // Process the item (example: log it)
+              jQuery
+                .ajax({
+                  url: `/game.php?screen=info_command&ajax=details&id=${item}`,
+                  dataType: "json",
+                })
+                .done((response) => {
+                  const { no_authorization } = response;
+                  if (no_authorization) {
+                    console.error(`Error:`, data);
+                  } else {
+                    console.log(response);
+                    results.push(response);
+                    // addRow(response);
+                  }
+                })
+                .fail((textStatus, errorThrown) => {
+                  console.error(
+                    `Request failed: ${textStatus}, ${errorThrown}`
+                  );
+                });
+            } else {
+              // Step 4: Clear the interval when the array is empty
+              clearInterval(timerId);
+              console.log("All items processed.");
+            }
+          }, 400);
+        } else {
+          UI.ErrorMessage("No commands found", $cc);
+        }
+      }
+    );
+  }
+
+  function updateDB() {
+    twSDK.fetchWorldData();
   }
 })();
